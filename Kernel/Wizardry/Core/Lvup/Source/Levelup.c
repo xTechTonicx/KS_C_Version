1f7665ad721fe39d18e0410490a4e1ee380300bf
xTechTonicx <coaster.ubc@gmail.com>
2025-05-14 22:35:52 -0600

    Fixed version control part 2

--- Kernel/Wizardry/Core/Lvup/Source/Levelup.c
+++ Kernel/Wizardry/Core/Lvup/Source/Levelup.c
@@ -30,7 +30,7 @@ STATIC_DECLAR void UnitLvup_Vanilla(struct BattleUnit *bu, int bonus)
 {
 	struct Unit *unit = GetUnit(bu->unit.index);
 
-	bu->changeHP  = GetStatIncrease(GetUnitHpGrowth(unit)  + bonus);
+	bu->changeHP = GetStatIncrease(GetUnitHpGrowth(unit) + bonus);
 	bu->changePow = GetStatIncrease(GetUnitPowGrowth(unit) + bonus);
 	bu->changeSkl = GetStatIncrease(GetUnitSklGrowth(unit) + bonus);
 	bu->changeSpd = GetStatIncrease(GetUnitSpdGrowth(unit) + bonus);
@@ -45,7 +45,7 @@ STATIC_DECLAR void UnitLvup_RandC(struct BattleUnit *bu, int bonus)
 {
 	struct Unit *unit = GetUnit(bu->unit.index);
 
-	bu->changeHP  = GetStatIncreaseRandC(GetUnitHpGrowth(unit)  + bonus);
+	bu->changeHP = GetStatIncreaseRandC(GetUnitHpGrowth(unit) + bonus);
 	bu->changePow = GetStatIncreaseRandC(GetUnitPowGrowth(unit) + bonus);
 	bu->changeSkl = GetStatIncreaseRandC(GetUnitSklGrowth(unit) + bonus);
 	bu->changeSpd = GetStatIncreaseRandC(GetUnitSpdGrowth(unit) + bonus);
@@ -65,20 +65,28 @@ STATIC_DECLAR void UnitLvup_Fixed(struct BattleUnit *bu, int bonus)
 	if (CA_PROMOTED & UNIT_CATTRIBUTES(unit))
 		ref = ref + 19;
 
-	bu->changeHP  = GetStatIncreaseFixed(GetUnitHpGrowth(unit)  + bonus, ref += 5);
-	bu->changePow = GetStatIncreaseFixed(GetUnitPowGrowth(unit) + bonus, ref += 5);
-	bu->changeSkl = GetStatIncreaseFixed(GetUnitSklGrowth(unit) + bonus, ref += 5);
-	bu->changeSpd = GetStatIncreaseFixed(GetUnitSpdGrowth(unit) + bonus, ref += 5);
-	bu->changeLck = GetStatIncreaseFixed(GetUnitLckGrowth(unit) + bonus, ref += 5);
-	bu->changeDef = GetStatIncreaseFixed(GetUnitDefGrowth(unit) + bonus, ref += 5);
-	bu->changeRes = GetStatIncreaseFixed(GetUnitResGrowth(unit) + bonus, ref += 5);
+	bu->changeHP =
+		GetStatIncreaseFixed(GetUnitHpGrowth(unit) + bonus, ref += 5);
+	bu->changePow =
+		GetStatIncreaseFixed(GetUnitPowGrowth(unit) + bonus, ref += 5);
+	bu->changeSkl =
+		GetStatIncreaseFixed(GetUnitSklGrowth(unit) + bonus, ref += 5);
+	bu->changeSpd =
+		GetStatIncreaseFixed(GetUnitSpdGrowth(unit) + bonus, ref += 5);
+	bu->changeLck =
+		GetStatIncreaseFixed(GetUnitLckGrowth(unit) + bonus, ref += 5);
+	bu->changeDef =
+		GetStatIncreaseFixed(GetUnitDefGrowth(unit) + bonus, ref += 5);
+	bu->changeRes =
+		GetStatIncreaseFixed(GetUnitResGrowth(unit) + bonus, ref += 5);
 
-	BU_CHG_MAG(bu) = GetStatIncreaseFixed(GetUnitMagGrowth(unit) + bonus, ref += 5);
+	BU_CHG_MAG(bu) =
+		GetStatIncreaseFixed(GetUnitMagGrowth(unit) + bonus, ref += 5);
 }
 
 STATIC_DECLAR void UnitLvup_100(struct BattleUnit *bu, int bonus)
 {
-	bu->changeHP  = 1;
+	bu->changeHP = 1;
 	bu->changePow = 1;
 	bu->changeSkl = 1;
 	bu->changeSpd = 1;
@@ -88,23 +96,25 @@ STATIC_DECLAR void UnitLvup_100(struct BattleUnit *bu, int bonus)
 	BU_CHG_MAG(bu) = 1;
 }
 
-STATIC_DECLAR void UnitLvup_0(struct BattleUnit *bu, int bonus) {}
+STATIC_DECLAR void UnitLvup_0(struct BattleUnit *bu, int bonus)
+{
+}
 
 STATIC_DECLAR void UnitLvupCore(struct BattleUnit *bu, int bonus)
 {
-	static void (*const funcs[])(struct BattleUnit *bu, int bonus) = {
-		[0] = UnitLvup_Vanilla,
-		[1] = UnitLvup_RandC,
-		[2] = UnitLvup_Fixed,
-		[3] = UnitLvup_100,
-		[4] = UnitLvup_0
-	};
+	static void (*const funcs[])(struct BattleUnit *bu,
+				     int bonus) = { [0] = UnitLvup_Vanilla,
+						    [1] = UnitLvup_RandC,
+						    [2] = UnitLvup_Fixed,
+						    [3] = UnitLvup_100,
+						    [4] = UnitLvup_0 };
 
 	int mode, total_lvup, i, retry;
 
 	if (TUTORIAL_MODE())
 		mode = gpKernelDesigerConfig->lvup_mode_tutorial;
-	if (gPlaySt.config.controller || (gPlaySt.chapterStateBits & PLAY_FLAG_HARD))
+	if (gPlaySt.config.controller ||
+	    (gPlaySt.chapterStateBits & PLAY_FLAG_HARD))
 		mode = gpKernelDesigerConfig->lvup_mode_hard;
 	else
 		mode = gpKernelDesigerConfig->lvup_mode_normal;
@@ -120,8 +130,9 @@ STATIC_DECLAR void UnitLvupCore(struct BattleUnit *bu, int bonus)
 	retry = gpKernelDesigerConfig->guaranteed_lvup ? 10 : 0;
 
 	for (i = 0; i < retry; i++) {
-		total_lvup = bu->changeHP + bu->changePow + bu->changeSkl + bu->changeSpd +
-						bu->changeLck + bu->changeDef + bu->changeRes + BU_CHG_MAG(bu);
+		total_lvup = bu->changeHP + bu->changePow + bu->changeSkl +
+			     bu->changeSpd + bu->changeLck + bu->changeDef +
+			     bu->changeRes + BU_CHG_MAG(bu);
 
 		if (total_lvup != 0)
 			break;
@@ -139,7 +150,9 @@ LYN_REPLACE_CHECK(CheckBattleUnitLevelUp);
 void CheckBattleUnitLevelUp(struct BattleUnit *bu)
 {
 	if (CanBattleUnitGainLevels(bu) && bu->unit.exp >= 100) {
-		int bonus = (bu->unit.state & US_GROWTH_BOOST) ? get_metis_tome_growth_bonus() : 0;
+		int bonus = (bu->unit.state & US_GROWTH_BOOST) ?
+				    get_metis_tome_growth_bonus() :
+				    0;
 
 		bu->unit.exp -= 100;
 		bu->unit.level++;
@@ -149,13 +162,14 @@ void CheckBattleUnitLevelUp(struct BattleUnit *bu)
 				bu->expGain -= bu->unit.exp;
 				bu->unit.exp = UNIT_EXP_DISABLED;
 			}
-		} else if (bu->unit.level == UNIT_LEVEL_MAX_RE ||
-				 UNIT_RECORDED_LEVEL_MAX == (bu->unit.level + GetUnitHiddenLevel(&bu->unit))) {
+		} else if (bu->unit.level == 20) {
 			bu->expGain -= bu->unit.exp;
 			bu->unit.exp = UNIT_EXP_DISABLED;
 		}
 
-		TryAddSkillLvup(GetUnitFromCharIdAndFaction(UNIT_CHAR_ID(&bu->unit), FACTION_BLUE), bu->unit.level);
+		TryAddSkillLvup(GetUnitFromCharIdAndFaction(
+					UNIT_CHAR_ID(&bu->unit), FACTION_BLUE),
+				bu->unit.level);
 		UnitLvupCore(bu, bonus);
 
 		CheckBattleUnitStatCaps(GetUnit(bu->unit.index), bu);
--- include/common-chax.h
+++ include/common-chax.h
@@ -19,7 +19,11 @@
 #endif
 
 #ifndef hang
-#define hang() do { while (1); } while (0)
+#define hang()            \
+	do {              \
+		while (1) \
+			; \
+	} while (0)
 #endif
 
 #define FORCE_DECLARE __attribute__((unused))
@@ -27,17 +31,17 @@
 #define NOINLINE __attribute__((noinline))
 
 #define LYN_REPLACE_CHECK(name) \
-	static const void *const _maybe_unused lyn_exists_check_ ## name = &name
+	static const void *const _maybe_unused lyn_exists_check_##name = &name
 
 /* Pointer valid judge */
-#define IS_IWRAM_PTR(ptr)    ((((u32)(ptr)) & 0xFF000000) == 0x03000000)
-#define IS_EWRAM_PTR(ptr)    ((((u32)(ptr)) & 0xFF000000) == 0x02000000)
-#define IS_ROM_PTR(ptr)      ((((u32)(ptr)) & 0xF8000000) == 0x08000000)
-#define IS_ROM_THUMB(ptr)    ((((u32)(ptr)) & 0xF8000001) == 0x08000001)
-#define IS_ROM_DATA(ptr)     ((((u32)(ptr)) & 0xF8000001) == 0x08000000)
+#define IS_IWRAM_PTR(ptr) ((((u32)(ptr)) & 0xFF000000) == 0x03000000)
+#define IS_EWRAM_PTR(ptr) ((((u32)(ptr)) & 0xFF000000) == 0x02000000)
+#define IS_ROM_PTR(ptr) ((((u32)(ptr)) & 0xF8000000) == 0x08000000)
+#define IS_ROM_THUMB(ptr) ((((u32)(ptr)) & 0xF8000001) == 0x08000001)
+#define IS_ROM_DATA(ptr) ((((u32)(ptr)) & 0xF8000001) == 0x08000000)
 #define IS_ANTI_HUFFMAN(ptr) ((((u32)(ptr)) & 0xF8000000) == 0x88000000)
 
-#define UNIT_LEVEL_MAX_RE 25
+#define UNIT_LEVEL_MAX_RE 20
 #define UNIT_RECORDED_LEVEL_MAX 80
 
 typedef bool (*HookProcFunc_t)(ProcPtr proc);
