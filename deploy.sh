b05945a65d7b3e93a79a4bbca6e863ac448a327e
xTechTonicx <coaster.ubc@gmail.com>
2025-05-17 18:16:03 -0600

    Small Changes

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
-cp fe8-kernel-dev.gba '/mnt/e/Users/Alec/Projects/Keystone Saga/C Skillsystem Output'
+if [ $1 -eq 0 ]
+    then cp fe8-kernel-dev.gba '/mnt/e/Users/Alec/Projects/Keystone Saga/C Skillsystem Output';
+elif [ $1 -eq 1 ]
+    then cp fe8-kernel-dev.gba '/mnt/c/Users/Alec/Desktop';
+fi
