496c28b8efe82ae97a8570970fb485a27b662885
xTechTonicx <coaster.ubc@gmail.com>
2025-05-21 21:29:39 -0600

    Removed personal caps

--- Data/MaxStatus/MaxStatus.c
+++ Data/MaxStatus/MaxStatus.c
@@ -1,14 +1,4 @@
 #include <common-chax.h>
 #include <status-getter.h>
 
-const struct UnitMaxStatusConfig gUnitMaxStatusConfigTable[0x100] = {
-	[CHARACTER_EIRIKA] = {
-		.pow = -2,
-		.mag = -2,
-		.spd = 5,
-		.skl = 5,
-		.lck = 5,
-		.def = -2,
-		.res = -2,
-	},
-};
+const struct UnitMaxStatusConfig gUnitMaxStatusConfigTable[0x100] = {};
