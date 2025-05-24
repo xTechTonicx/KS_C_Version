34f0e874c05c5df198b1ee0617897f50cde9d1df
alec_776 <alecf.currie@gmail.com>
2025-05-24 15:45:49 -0600

    Changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ $1 -eq 0 ]
     then cp fe8-kernel-dev.gba '/mnt/e/Users/Alec/Projects/Keystone Saga/C Skillsystem Output';
+    cp fe8-kernel-dev.sym '/mnt/e/Users/Alec/Projects/Keystone Saga/C Skillsystem Output';
 elif [ $1 -eq 1 ]
     then cp fe8-kernel-dev.gba '/mnt/c/Users/Alec/Desktop';
 fi
