5253c7940764104538f4ce1eb3103e4693085e51
TechTonic <coaster.ubc@gmail.com>
2025-07-08 23:47:10 -0600

    Removed crit bonuses

--- Data/BattleSys/Source/CriticalBonus.c
+++ Data/BattleSys/Source/CriticalBonus.c
@@ -2,14 +2,9 @@
 #include "battle-system.h"
 
 const s8 gCriticalBonus[0x100] = {
-	[CLASS_SWORDMASTER] = 15,
-	[CLASS_SWORDMASTER_F] = 15,
-	[CLASS_BERSERKER] = 10,
+
 };
 
 const s8 gSilencerBonus[0x100] = {
-	[CLASS_SWORDMASTER] = 1,
-	[CLASS_SWORDMASTER_F] = 1,
-	[CLASS_ASSASSIN] = 20,
-	[CLASS_ASSASSIN_F] = 20,
+	
 };
